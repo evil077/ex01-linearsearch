59b208e9de169bab608084453b42d915e18e7cc8
VC Science Labs <vscilab@vcccd.edu>
2018-09-05 14:59:50 -0700

    testing testin 123

---
+++ src/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "linearsearch.h"
+
+using edu::vcccd::vc::csv15::find;
+using std::cout;
+using std::endl;
+int main(int argc, char *argv[]) {
+	int array[] = {3, 1, 2};
+	int64_t index = find(0, array, 3);
+
+	double doubles[] = {3.0, 1.0, 2.0};
+	find(0.0, doubles, 3);
+
+	cout << "The index value is " << index << endl;
+}
